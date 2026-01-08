d6a3797dd52fbc5a1147783646d1b3bc02a526b0
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-01-08 22:10:22 +0530

    Add files via upload

---
+++ loop3.java
@@ -0,0 +1,52 @@
+/*public class loop3 {
+    public static void main(String args[]){
+        for(int i = 1 ; i <=4 ; i++){//Star Pattern
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}*/
+
+//Inverted Star Pattern
+/*public class loop3{
+    public static void main(String args[]){
+        int n = 4 ;
+        for(int i = 1 ; i<=4 ; i++){
+            for(int j = 1 ; j<=n-i+1 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//Half Pyramid Pattern
+/*public class loop3{
+    public static void main(String args[]){
+        for(int i = 1 ; i<=4 ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//Character Pattern
+/*public class loop3{
+    public static void main(String args[]){
+        char ch = 'A';
+        for(int i = 1 ; i<=4 ; i++){
+            for(int j = 1  ; j<= i  ; j++){
+                System.err.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+}*/
+
