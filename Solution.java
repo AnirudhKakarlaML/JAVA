75279551ff492b5e9c06ceca5ebf71cc5666c40c
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-01-09 14:55:55 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,62 @@
+/*public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}*/
+
+/*import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/*public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N % 2 != 0){
+            System.out.println("Weird");
+        }
+        else if(N % 2 == 0 && N>=2 && N<=5){
+            System.out.println("Not Weird");
+        }
+        else if(N % 2 == 0 && N>=6 && N<=20){
+            System.out.println("Weird");
+        }
+        else{
+            System.out.println("Not Weird");
+        }
+        
+ 
+        scanner.close();
+    }
+}*/
+
+
+
+
