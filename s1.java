843091928aed056b9be1b179e45b1da2cd2329be
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-02-09 22:11:38 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,53 @@
+/*public class s1 {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+    
+}*/
+
+//Prime Number Check
+/*public class s1{
+    public static boolean isPrime(int n){
+        boolean Prime = true;
+        if(n == 1){
+            System.out.println("Neither Prime Nor Composite");
+        }
+        for(int i = 2 ; i<n ; i++){
+            if(n % i == 0){
+                Prime = false;
+            }
+        }
+        return Prime;
+    }
+    public static void main(String args[]){
+        System.out.println(isPrime(7));
+    }
+}*/
+
+/*public class s1{
+    public static boolean isPrime(int n){
+        boolean Prime = true;
+        if(n == 1){
+            System.out.println("Neither Prime Nor Composite");
+        }
+        for(int i = 2 ; i<n ; i++){
+            if(n % i == 0){
+                Prime = false;
+            }
+        }
+        return Prime;
+    }
+    public static void primerange(int n){
+        for(int i =2 ; i<=n ; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+            
+        }
+    }
+    public static void main(String args[]){
+        primerange(7);
+    }
+}*/
+
+
